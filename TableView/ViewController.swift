91cf320b81397faecffb549c4dcfd133a4ded8e0
Pedro Moura <pedro.moura@socialbank.com.br>
2019-10-22 14:51:04 -0300

    Learning tableview

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let cellIdentifier = "CellIdentifier"
+    var fruits: [String] = []
+    var alphabetizedFruits = [String: [String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        fruits = ["Apple", "Pineapple", "Orange", "Blackberry", "Banana", "Pear", "Kiwi", "Strawberry", "Mango", "Walnut", "Apricot", "Tomato", "Almond",            "Date", "Melon", "Water Melon", "Lemon", "Coconut", "Fig", "Passionfruit", "Star Fruit", "Clementin", "Citron", "Cherry", "Cranberry"]
     }
 
-
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //MAKE: Number Rows in TableViewCell
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    //MAKE: Render View in Cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+       let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+       // Fetch Fruit
+       let fruit = fruits[indexPath.row]
+        
+       // Configure Cell
+       cell.textLabel?.text = fruit
+        
+       return cell
+    }
+    
+    //MAKE: Cell Selected
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(fruits[indexPath.row])
+    }
 }
 
